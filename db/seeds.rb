c7792a62a969dd09d1a72665e606e62d6e0b954c
Dillon Cortez <dilloncortez@gmail.com>
2015-04-14 15:39:03 -0600

    Revert "errything"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,7 @@
-User.create!(first_name:  "Example",
-             last_name: "User",
-             email: "example@example.com",
-             password:              "password",
-             password_confirmation: "password")
-
-9.times do |n|
-  first_name  = Faker::Name.first_name
-  last_name  = Faker::Name.last_name
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(first_name:  first_name,
-               last_name:   last_name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
-
-users = User.all
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
