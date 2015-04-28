74dcd17e744109c2f4dcace554e0ebb5053b5144
grandmasterspoc <dilloncortez@gmail.com>
2015-04-27 20:19:44 -0600

    Updated songs controller to array that holds mixes..I hope

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,13 +5,9 @@ class Song < ActiveRecord::Base
   acts_as_mentionable
 
   def combine_mixes(*args)
-    args.each do |song|
-      f = File.open(song, 'rb')
-      # @s = f.read
-      binding.pry
-     end
-    # output = @s.write(args.each)
-    # puts output.inspect
+    song = []
+    song<<args 
+    return song
   end
 
 end
